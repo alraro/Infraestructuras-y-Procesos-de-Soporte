b28dc88795c02dfe6eb68d3e172952fc43655b60
alraro <alfonsoramosrojas@gmail.com>
2026-06-10 12:05:50 +0200

    Terminada parte 5

--- practica10/practicaReal/main.tf
+++ practica10/practicaReal/main.tf
@@ -13,3 +13,13 @@ module "frontend-app" {
   author       = "alraro el fronted slop creator"
   environment  = "dev"
 }
+
+module "projects-with-foreach" {
+  source   = "./modules/project"
+  for_each = var.projects
+
+  project_name = each.key
+  author       = each.value.author
+  environment  = each.value.environment
+  base_dir     = var.map_projects_base_dir
+}
--- practica10/practicaReal/variables.tf
+++ practica10/practicaReal/variables.tf
@@ -3,3 +3,30 @@ variable "projects_base_dir" {
   description = "Ruta de todos los proyectos generados"
   default     = "./projects"
 }
+
+variable "projects" {
+  type = map(object({
+    author      = string
+    environment = string
+  }))
+  default = {
+    "modelo-machine-learning" = {
+      author      = "Alfonso el programador de ia"
+      environment = "dev"
+    }
+    "videojuego" = {
+      author      = "Alfonso el sucesor de Alva Majo"
+      environment = "prod"
+    }
+    "compilador" = {
+      author      = "Alfonso el programador de bajo nivel"
+      environment = "staging"
+    }
+  }
+}
+
+variable "map_projects_base_dir" {
+  type        = string
+  description = "Ruta de todos los proyectos generados (usando foreach)"
+  default     = "./map_projects"
+}
